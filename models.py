7b05e47e4bde5f3b8eeb6f9f9c2b8cc9f9862f03
Tomas Neme <lacrymology@gmail.com>
2011-05-05 21:22:14 -0300

    created models and added the templates dir

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Entry(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    address = models.ForeignKey("Address")
+
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
