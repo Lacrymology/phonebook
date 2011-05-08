bd7f1adaf1224e60a54d33978dcc5d9dc3a5603f
Tomas Neme <lacrymology@gmail.com>
2011-05-08 13:00:48 -0300

    changed the models a bit, and the default template

--- models.py
+++ models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Entry(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=50)
-    address = models.ForeignKey("Address")
-
 class Address(models.Model):
     street = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
+    entry = models.OneToOneField("Entry")
+
+class Entry(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    #address = models.ForeignKey(Address)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+        verbose_name_plural = "entries"
+
+    def __unicode__(self):
+        return "%s %s"%(self.last_name, self.first_name)
--- views.py
+++ views.py
@@ -1,1 +1,14 @@
+from phonebook.models import Entry
 # Create your views here.
+
+# these are not actually views, but rather the functions to
+# retrieve the context needed in your views
+
+def getAllEntries():
+    return Entry.objects.all()
+
+def getAllNames():
+    return map(lambda x: x.name, Entry.objects.all())
+
+def getDefaultData():
+    return {'entry_list': getAllEntries()}
