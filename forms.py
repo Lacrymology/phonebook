ea4d899277611a3224b7b55228b988ca64f7d33c
Tomas Neme <lacrymology@gmail.com>
2011-05-13 14:30:38 -0300

    modified forms so they are able to show the two forms separately

--- forms.py
+++ forms.py
@@ -1,5 +1,12 @@
 from django import forms
-from phonebook.models import Entry
+from phonebook.models import Entry, Address
 
 class EntryForm(forms.ModelForm):
-    model = Entry
+    class Meta:
+        model = Entry
+        exclude = ('user')
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        exclude = ('entry')
